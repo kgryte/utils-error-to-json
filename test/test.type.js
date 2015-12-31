2ce0cde6bff3f63e6ece4f070a14a373a835c617
kgryte <kgryte@gmail.com>
2015-12-30 22:05:27 -0800

    Remove additional instance check

--- test/test.type.js
+++ test/test.type.js
@@ -13,28 +13,6 @@ test( 'file exports a function', function test( t ) {
 	t.end();
 });
 
-test( 'if not provided an error instance, the function returns null', function test( t ) {
-	var values;
-	var i;
-
-	values = [
-		'5',
-		5,
-		null,
-		NaN,
-		undefined,
-		true,
-		[],
-		{},
-		function(){}
-	];
-
-	for ( i = 0; i < values.length; i++ ) {
-		t.equal( typeName( values[ i ] ), null, 'returns `null` when provided a ' + ( typeof values[ i ] ) );
-	}
-	t.end();
-});
-
 test( 'if provided an error or error subclass, the function returns the closest error type', function test( t ) {
 	var expected;
 	var values;
